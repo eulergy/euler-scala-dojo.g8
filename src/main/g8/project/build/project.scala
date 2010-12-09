3cd96129ee617f91ea200c1fe9c25915f8f9b316
chrislewis <chris@thegodcode.net>
2010-12-09 13:41:53 -0500

    scala 2.8 + specs 1.6.5

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class MyProject(info: ProjectInfo) extends DefaultProject(info) {
-  val specs = "org.scala-tools.testing" %% "specs_2.8.0.Beta1" %"1.6.2.2" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" %"1.6.5" % "test"
 }
