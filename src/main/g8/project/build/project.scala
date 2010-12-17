bfced7a30cb731db2d4b3dd4e0ca6001aec7d114
Dan Oxlade <dan.oxlade@gmail.com>
2010-12-17 19:30:48 +0000

    template seems to work

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -1,5 +1,10 @@
 import sbt._
 
 class MyProject(info: ProjectInfo) extends DefaultProject(info) {
-  val specs = "org.scala-tools.testing" %% "specs" %"1.6.5" % "test"
+  	val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+  	val scalaSnapshots = "Scala Snapshots repo" at "http://scala-tools.org/repo-snapshots"	
+
+	val junit = "junit" % "junit" % "4.7" % "test" withSources
+	val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7-SNAPSHOT" % "test" withSources
+	val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources
 }
---
+++ src/main/g8/src/test/scala/EulerSpec.scala
@@ -0,0 +1,11 @@
+import org.specs._
+
+class EulerSpec extends Specification {
+  
+  "Euler Problem Number $problemNumber$" should {
+    "$problemDescription$" in {
+
+    }
+  }
+  
+}
